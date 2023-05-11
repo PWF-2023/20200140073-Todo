93601bb4e4d68538b404cf2850c901e25f9f370d
Zahran Rafif <zahranlumia@gmail.com>
2023-05-11 19:08:21 +0700

    update, delete data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -15,7 +15,10 @@ class TodoController extends Controller
             ->get();
 
         //dd($todos);
-        return view('todo.index', compact('todos'));
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_complete', true)
+            ->count();
+        return view('todo.index', compact('todos', 'todosCompleted'));
     }
 
     public function store(Request $request, Todo $todo)
@@ -36,12 +39,15 @@ class TodoController extends Controller
         //'user_id' => auth()->user()->id,
         //'created_at' => now(),
         //'updated_at' => now(),
-        //])
-        $todo = Todo::create([
-            'title' => ucfirst($request->title),
-            'user_id' => auth()->user()->id,
-        ]);
-        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
+        //]);
+
+        // Eloqvent way - Shortest
+        $request->user()->todos()->create($request->all());
+        // $request->user()->todos()->create([
+        //     'title' => ucfirst($request->title),
+        // ]);
+
+        return redirect()->route('todo.index')->with('success', 'Todo create successfully');
     }
 
     public function create()
@@ -49,8 +55,77 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function edit()
+    public function edit(Todo $todo)
     {
-        return view('todo.edit');
+        if (auth()->user()->id == $todo->user_id) {
+            // dd($todo);
+            return view('todo.edit', compact('todo'));
+        } else {
+            // abort(403);
+            // abort(403, 'Not authorized')
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to edit this todo!');
+        }
+    }
+
+    public function update(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        // Practical
+        // $todo->title = $request->title;
+        // $todo->save();
+
+        // Elequent Way - Readble
+        $todo->update([
+            'title' => ucfirst($request->title),
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo update successfully');
+    }
+
+    public function complete(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->update([
+                'is_complete' => true,
+            ]);
+            return redirect()->route('todo.index')->with('success', 'Todo comleted successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to complete this todo!');
+        }
+    }
+    public function uncomplete(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->update([
+                'is_complete' => false,
+            ]);
+            return redirect()->route('todo.index')->with('success', 'Todo uncomleted successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to complete this todo!');
+        }
+    }
+    public function destroy(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->delete();
+            return redirect()->route('todo.index')->with('success', 'Todo deleted successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to delete this todo!');
+        }
+    }
+
+    public function destroyCompleted()
+    {
+        // get all todos for current user where is_complete is true
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_complete', true)
+            ->get();
+        foreach ($todosCompleted as $todo) {
+            $todo->delete();
+        }
+        // dd($todosCompleted);
+        return redirect()->route('todo.index')->with('success', 'All completed todos deleted successfully!');
     }
 }
---
+++ tugas6.md
@@ -0,0 +1,26 @@
+# Tugas 6
+# Zahran Rafif
+
+## 1. Todo Completed
+![Alt text](screenshot/tugas6/todocompleted.png)
+
+## 2. Todo Uncompleted
+![Alt text](screenshot/tugas6/todouncompleted.png)
+
+## 3. Todo Edited
+![Alt text](screenshot/tugas6/todoedited.png)
+
+## 4. Todo Deleted
+![Alt text](screenshot/tugas6/tododeleted.png)
+
+## 5. Todo Deleted All Completed Todo
+![Alt text](screenshot/tugas6/tododeletedall.png)
+
+## 6. User Make Admin
+![Alt text](screenshot/tugas6/makeadmin.png)
+
+## 7. User Remove Admin
+![Alt text](screenshot/tugas6/removeadmin.png)
+
+## 8. User Deleted
+![Alt text](screenshot/tugas6/userdeleted.png)
